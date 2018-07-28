4261e7c8827a7d439e80b260f82fe8a143f993cc
Shawn Poulson <spoulson@sevone.com>
2018-07-27 23:51:12 -0400

    Add resizer script.

---
+++ bash/resizer/resizer.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Resize directory of images to a fixed width with aspect.
+# Randomize filenames.
+# Useful for processing a set of images for website and concealing original names.
+
+# For Mac OSX, install ImageMagick: brew install imagemagick
+
+WIDTH=960
+
+mkdir -p resized
+
+for filename in *.jpg; do
+    NEWNAME=$(cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | head -c 8)
+    convert "$filename" -resize 1200x -write resized/"${NEWNAME}"_960x.jpg
+done
